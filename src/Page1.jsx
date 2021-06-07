92691983ac288571917bc8bafa8bc620e5585399
jinbo-0523 <65935770+jinbo-0523@users.noreply.github.com>
2021-06-07 19:18:37 +0900

    ネストされた遷移ページ

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 export const Page1 = () => {
   return (
     <div>
       <h1>Page1ページです</h1>
+      <Link to="/page1/detailA">Page1 DetailA</Link>
+      <br />
+      <Link to="/page1/detailB">Page1 DetailB</Link>
+      <br />
     </div>
   );
 };
